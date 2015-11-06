5eb4b416c8376cfb3f75d13a8a97f899de20b841
sonnh123 <sonnh@kaopiz.com>
2015-11-06 22:47:38 +0700

    search function

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -19,22 +19,9 @@
 		
 		
         public function getSearch(){
-            $type = Input::get('type');
-        	$title = Input::get('title');
-        	// $data= DB::table('images')->where('title','like','%'.$key.'%')
-        	// 						  ->orwhereIn('images.album_id',function($query)
-        	// 						  {
-        	// 						  	$query->select(DB::raw('id'))
-        	// 						  		  ->from('albums')
-        	// 						  		  ->where('title','like','%'.Input::get('title').'%');
-        	// 						  })
-        	// 						  ->get();
-            $data['products'] = Product::select('*')->where($type,'like','%'.$title.'%')->where('public','=','1')->get();
+            $title = Input::get('title');
+        	$data['products'] = Product::select('*')->where('title','like','%'.$title.'%')->where('public','=','1')->get();
             return View::make('frontend/index')->with('data',$data);
-
-
-//         	SELECT * FROM `images` where images.`title` LIKE '%t%' OR images.album_id IN 
-// (SELECT id FROM albums WHERE `title` LIKE '%t%')
         }
         
         public function postDoSignup(){
